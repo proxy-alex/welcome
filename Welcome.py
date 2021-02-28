bcff8048a0c2324b146498fe1fc85db66fc6fc8a
proxy-alex <74700207+proxy-alex@users.noreply.github.com>
2021-02-28 13:02:57 +0530

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,23 @@
+import amino
+
+email = ""  # Set your own password here!
+password = ""  # Set your own password here!
+cid = ""  # Community ID
+
+client = amino.Client()
+client.login(email=email, password=password)
+print("Bot logged in")
+sub = amino.SubClient(comId=cid, profile=client.profile)
+print("Bot logged onto the community, id:", cid, "\nBot Name:", sub.profile.nickname)
+
+join = """welcome in the chat"""
+
+@client.callbacks.event('on_group_member_join')
+def on_group_member_join(data):
+        nick = data.message.author.nickname
+        msg = {
+        'message': f"{join}\n\n[CB]{nick} welcome 🌹🌹🌹🌹!!",
+        'chatId': data.message.chatId,
+        'mentionUserIds': [data.message.author.userId]
+        }
+        sub.send_message(**msg)
